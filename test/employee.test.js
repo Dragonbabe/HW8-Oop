8b6a0e0c50bb6e28eb6af8bdeb85d6289dea8919
Esther Walker <equestriennesrule@yahoo.com>
2020-02-03 17:14:27 -0800

    Succesful test of id property of the emp class

---
+++ test/employee.test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Employee = require("../lib/employee");
+
+describe("Employee", () => {
+    describe('initialization', () => {
+        it('should create an object with an id property', () => {
+            //arrange and act
+            const employee = new Employee(0);
+            //assert
+            expect(employee.id).toEqual(0);
+
+        });
+
+    });
+
+});
