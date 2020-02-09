66ddcabe172f10c4d109e1756dfc87d67dd82d49
Esther Walker <equestriennesrule@yahoo.com>
2020-02-08 22:01:18 -0800

    got my app js working

--- app.js
+++ app.js
@@ -45,49 +45,23 @@ const roleQuestions = [
     }
 ];
 
-const addMore = [
-    {
-        type: "list",
-        message: "Add more employee?",
-        choices: ["Yes", "No"],
-        name: "addMore"
-    }
-];
-
-
-async function addEmployee() {
-    let addEmployee = true;
-    let employeeArr = [];
-    while (addEmployee) {
-        const userInfo = await inquirer.prompt(questions);
-        console.log(userInfo.name)
-        console.log(userInfo.role);
-        if (userInfo.role === 'manager') {
-            userInfo.roleInfo = await inquirer.prompt(roleQuestions[0]);
-        } else if (userInfo.role === 'engineer') {
-            userInfo.roleInfo = await inquirer.prompt(roleQuestions[1]);
-        } else {
-            userInfo.roleInfo = await inquirer.prompt(roleQuestions[2]);
-        }
-        employeeArr.push(userInfo);
-        let addMore = await inquirer.prompt(addMore);
-        if (addMore.addMore === "No") {
-            addEmployee = false;
-        }
-    }
-
-    return employeeArr;
-}
-
 async function init() {
-    try {
-        // userInfo is an obj, consist of user's inputs from questions.
-        const userInfo = await addEmployee();
-        console.log(userInfo);
-    } catch {
+    const userInfo = await inquirer.prompt(questions);
 
+    if (userInfo.role === 'manager') {
+        userInfo.roleInfo = await inquirer.prompt(roleQuestions[0]);
+    } else if (userInfo.role === 'engineer') {
+        userInfo.roleInfo = await inquirer.prompt(roleQuestions[1]);
+    } else {
+        userInfo.roleInfo = await inquirer.prompt(roleQuestions[2]);
     }
+};
 
-}
+
+
+
+
+
+
 
 init();
