68120a89d061c786af7d321d5f45f66e273f9413
Esther Walker <equestriennesrule@yahoo.com>
2020-02-08 23:32:06 -0800

    added html file to my javascript file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
 const path = require('path');
 const inquirer = require('inquirer');
+const util = require('util');
+const fs = require('fs');
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 const questions = [
     {
@@ -56,17 +60,130 @@ async function init() {
         } else {
             userInfo.roleInfo = await inquirer.prompt(roleQuestions[2]);
         }
-        await writeFileAsync('team.html', generateHTML(userInfo.roleInfo.name.id.email.response), 'utf8');
+        await writeFileAsync('team.html', generateHTML(userInfo), 'utf8');
 
     } catch (err) {
         console.error(err);
     };
 
+};
+
+async function generateHTML() {
+    const page = await browser.newPage();
+    await page.goto(`file://${path.resolve(__dirname, 'team.html')}`, {
+        waitUntil: "networkidle0"
+    });
+};
+
+function generateHTML() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>People that work!</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <style>
+            .container {
+                display: flex;
+                justify-content: center;
+                flex-direction: row;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="parent-container">
+            <div class="box">
+                <h2 class="header"></h2>
+                <div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card text-center">
+                                <div class="card-header">
+                                    Welcome To.....
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">People That Work!</h5>
+                                    <p class="card-text">The Dream Team</p>
+                                </div>
+    
+                                <div class="card-footer">
+                                    <a href="#"></a>
+                                    <a href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="parent-container2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class="card" style="width: 18rem;">
+                                    <div class="card-header">
+                                        ${questions.name.role}
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">${questions.id}</li>
+                                        <li class="list-group-item">${questions.email}</li>
+                                        <li class="list-group-item">${questionsresponse}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class="card" style="width: 18rem;">
+                                    <div class="card-header">
+                                        ${questions.name.role}
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">${questions.id}</li>
+                                        <li class="list-group-item">${questions.email}</li>
+                                        <li class="list-group-item">${questions.response}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="parent-container3">
+            <div class="container">
+                <div class="row">
+    
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class="card" style="width: 18rem;">
+                                    <div class="card-header">
+                                        ${questions.name.role}
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">${questions.id}</li>
+                                        <li class="list-group-item">${questions.email}</li>
+                                        <li class="list-group-item">${questions.response}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </body>
+    </html>`
 }
-
-
-
-
-
 
 init();
