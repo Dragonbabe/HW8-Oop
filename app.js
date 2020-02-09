4cab0fdaa63fae007077c95ca3007d4dfcbe7950
Esther Walker <equestriennesrule@yahoo.com>
2020-02-08 21:51:29 -0800

    got all those html files and also my app.js

--- app.js
+++ app.js
@@ -1,1 +1,93 @@
 'use strict';
+const path = require('path');
+const inquirer = require('inquirer');
+
+const questions = [
+    {
+        type: 'input',
+        message: "what is the employee's name?",
+        name: "name"
+    },
+    {
+        type: 'input',
+        message: 'what is your employee id?',
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "what is your e-mail?",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "what is your role?",
+        choices: ["manager", 'engineer', 'intern'],
+        name: "role"
+    }
+
+];
+
+const roleQuestions = [
+    {
+        type: 'input',
+        message: 'what is your office number?',
+        name: 'response'
+    },
+    {
+        type: 'input',
+        message: ' what is your github id?',
+        name: 'response'
+    },
+    {
+        type: 'input',
+        message: 'Where did you go to school?',
+        name: 'response'
+    }
+];
+
+const addMore = [
+    {
+        type: "list",
+        message: "Add more employee?",
+        choices: ["Yes", "No"],
+        name: "addMore"
+    }
+];
+
+
+async function addEmployee() {
+    let addEmployee = true;
+    let employeeArr = [];
+    while (addEmployee) {
+        const userInfo = await inquirer.prompt(questions);
+        console.log(userInfo.name)
+        console.log(userInfo.role);
+        if (userInfo.role === 'manager') {
+            userInfo.roleInfo = await inquirer.prompt(roleQuestions[0]);
+        } else if (userInfo.role === 'engineer') {
+            userInfo.roleInfo = await inquirer.prompt(roleQuestions[1]);
+        } else {
+            userInfo.roleInfo = await inquirer.prompt(roleQuestions[2]);
+        }
+        employeeArr.push(userInfo);
+        let addMore = await inquirer.prompt(addMore);
+        if (addMore.addMore === "No") {
+            addEmployee = false;
+        }
+    }
+
+    return employeeArr;
+}
+
+async function init() {
+    try {
+        // userInfo is an obj, consist of user's inputs from questions.
+        const userInfo = await addEmployee();
+        console.log(userInfo);
+    } catch {
+
+    }
+
+}
+
+init();
--- test/manager.test.js
+++ test/manager.test.js
@@ -2,20 +2,29 @@
 
 const Manager = require('../lib/Manager');
 
-test('Can set office number via constructor argument', () => {
-    const testValue = 100;
-    const manager = new Manager('Foo', 1, 'test@test.com', testValue);
-    expect(manager.officeNumber).toBe(testValue);
-});
+describe('Manager', () => {
 
-test('getRole() should return "Manager"', () => {
-    const testValue = 'Manager';
-    const manager = new Manager('Foo', 1, 'test@test.com', 100);
-    expect(manager.getRole()).toBe(testValue);
-});
+    let manager;
 
-test('Can get office number via getOffice()', () => {
-    const testValue = 100;
-    const manager = new Manager('Foo', 1, 'test@test.com', testValue);
-    expect(manager.getOfficeNumber()).toBe(testValue);
+    beforeEach(() => {
+        manager = new Manager("foo", 1, "test@test.com", 100)
+    });
+    test('Can set office number via constructor argument', () => {
+        const testValue = 100;
+        console.log(manager);
+        expect(manager.officenumber).toBe(testValue);
+    });
+
+    test('getRole() should return "Manager"', () => {
+        const testValue = 'Manager';
+
+        expect(manager.getRole()).toBe(testValue);
+    });
+
+    test('Can get office number via getOffice()', () => {
+        const testValue = 100;
+
+        expect(manager.getOfficeNumber()).toBe(testValue);
+    });
+
 });
